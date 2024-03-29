e7c24d0cc6c10c6559d1b5a0099de1d7ea5fe148
hrishisd <hsdharam@gmail.com>
2024-03-28 23:25:01 -0400

    fix most bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use rand::{self, random, seq::index};
 use std::panic;
 mod font;
 
 pub struct Chip8 {
     /// Program should be loaded into memory starting at 0x200 (512)
     memory: [u8; 4096],
+    /// 64 pixels wide, 32 pixels tall
     /// indexed as `[row][col]` or `[y][x]`
     pub display: [[bool; 64]; 32],
     /// points to the current instruction in memory
@@ -15,7 +15,7 @@ pub struct Chip8 {
     index_reg: u16,
     /// Used to call and return from subroutines
     stack: Vec<u16>,
-    /// TODO: timer loop decrement is separate from emulation speed
+    /// timer loop decrement is separate from emulation speed
     /// decremented at 60 Hz until it reaches 0
     delay_timer: u8,
     /// decremented like delay timer
@@ -25,6 +25,12 @@ pub struct Chip8 {
     /// also called V0 to VF
     /// VF is also used as a flag register
     registers: [u8; 16],
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum DisplayState {
+    Updated,
+    NotUpdated,
 }
 
 impl Chip8 {
@@ -56,7 +62,7 @@ impl Chip8 {
         }
     }
 
-    pub fn step(&mut self, key: Option<char>) {
+    pub fn step(&mut self, keypresses: [bool; 16]) -> DisplayState {
         // fetch
         let first_byte = self.memory[self.pc as usize];
         let second_byte = self.memory[self.pc as usize + 1];
@@ -66,23 +72,24 @@ impl Chip8 {
         // decode
         let first_half_byte = first_byte >> 4;
         // second half-byte. used to look up one of the 16 registers.
-        let X = first_byte as usize & 0x0f;
+        let x = first_byte as usize & 0x0f;
         // third half-byte. used to look up one of the 16 registers.
-        let Y = second_byte as usize >> 4;
+        let y = second_byte as usize >> 4;
         // fourth half-byte
-        let N = second_byte & 0x0f;
+        let n = second_byte & 0x0f;
         // The second byte
-        let NN = second_byte;
+        let nn = second_byte;
         // a 12-bit immediate memory address, comprised of half-bytes 2-4
-        let NNN = inst & 0x0fff;
+        let nnn = inst & 0x0fff;
 
         // execute
         match first_half_byte {
             0x0 => {
-                match NNN {
+                match nnn {
                     0x0E0 => {
                         // clear screen
                         self.display = [[false; 64]; 32];
+                        return DisplayState::Updated;
                     }
                     0x0EE => {
                         self.pc = self.stack.pop().expect("Can't return from function call without a return address on the stack.");
@@ -93,104 +100,113 @@ impl Chip8 {
             0x1 => {
                 // 1NNN
                 // jump
-                self.pc = NNN;
+                self.pc = nnn;
             }
             0x2 => {
                 // call function at address NNN
                 // push the return address onto the stack first
                 self.stack.push(self.pc);
-                self.pc = NNN;
+                self.pc = nnn;
             }
             0x3 => {
                 // conditional skip
-                if self.registers[X] == NN {
+                if self.registers[x] == nn {
                     self.pc += 2;
                 }
             }
             0x4 => {
                 // conditional skip
-                if self.registers[X] != NN {
+                if self.registers[x] != nn {
                     self.pc += 2;
                 }
             }
             0x5 => {
                 // conditional skip
-                if self.registers[X] == self.registers[Y] {
+                if self.registers[x] == self.registers[y] {
                     self.pc += 2;
                 }
             }
             0x6 => {
                 // 6XNN
                 // set register VX
-                self.registers[X] = NN
+                self.registers[x] = nn
             }
             0x7 => {
                 // 7XNN
                 // add value to register Vx
-                if let Some(sum) = self.registers[X].checked_add(NN) {
-                    self.registers[X] = sum;
-                }
+                self.registers[x] = self.registers[x].wrapping_add(nn);
             }
             0x8 => {
                 // logical and arithmetic instructions operating on X and Y
                 // behavior toggled by last half-byte
-                match N {
+                match n {
                     // set
-                    0x0 => self.registers[X] = self.registers[Y],
+                    0x0 => self.registers[x] = self.registers[y],
                     // or
-                    0x1 => self.registers[X] |= self.registers[Y],
+                    0x1 => self.registers[x] |= self.registers[y],
                     // and
-                    0x2 => self.registers[X] &= self.registers[Y],
+                    0x2 => self.registers[x] &= self.registers[y],
                     // xor
-                    0x3 => self.registers[X] ^= self.registers[Y],
+                    0x3 => self.registers[x] ^= self.registers[y],
                     // add
-                    0x4 => self.registers[X] = self.registers[X].wrapping_add(self.registers[Y]),
+                    0x4 => {
+                        {
+                            let prev_val = self.registers[x];
+                            self.registers[x] = prev_val.wrapping_add(self.registers[y]);
+                            // check for overflow
+                            if prev_val > self.registers[x] {
+                                self.registers[0xf] = 1;
+                            } else {
+                                self.registers[0xf] = 0;
+                            }
+                        }
+                    }
                     // subtract
-                    0x5 => self.registers[X] = self.registers[X].wrapping_sub(self.registers[Y]),
-                    0x7 => self.registers[X] = self.registers[Y].wrapping_sub(self.registers[X]),
+                    0x5 => self.registers[x] = self.registers[x].wrapping_sub(self.registers[y]),
+                    0x7 => self.registers[x] = self.registers[y].wrapping_sub(self.registers[x]),
                     // shift
                     // (Optional, or configurable) Set VX to the value of VY
                     0x6 => {
                         // set flag register to low bit
-                        self.registers[0xF] = self.registers[X] & 0x1;
-                        self.registers[X] >>= 1;
+                        self.registers[0xF] = self.registers[x] & 0x1;
+                        self.registers[x] >>= 1;
                     }
                     0xE => {
                         // set flag register to high bit
-                        self.registers[0xF] = self.registers[X] & 0xA0;
-                        self.registers[X] <<= 1;
+                        self.registers[0xF] = self.registers[x] & 0x80;
+                        self.registers[x] <<= 1;
                     }
                     _ => panic!("Invalid instruction: {inst:#x}"),
                 }
             }
             0x9 => {
                 // conditional skip
-                if self.registers[X] != self.registers[Y] {
+                if self.registers[x] != self.registers[y] {
                     self.pc += 2;
                 }
             }
             0xa => {
                 // ANNN
                 // set index register to NNN
-                self.index_reg = NNN;
+                self.index_reg = nnn;
             }
             0xb => {
                 // jump with offset
-                self.pc = NNN + self.registers[0] as u16;
+                self.pc = nnn + self.registers[0] as u16;
             }
             0xc => {
                 // random
-                self.registers[X] = rand::random::<u8>() & NN;
+                self.registers[x] = rand::random::<u8>() & nn;
             }
             0xd => {
                 // DXYN
                 // draw
-                let mut y_coord = self.registers[Y] as usize % 32;
+                let mut y_coord = self.registers[y] as usize % 32;
                 self.registers[0xF] = 0;
                 let bytes =
-                    &self.memory[self.index_reg as usize..(self.index_reg as usize + N as usize)];
+                    &self.memory[self.index_reg as usize..(self.index_reg as usize + n as usize)];
                 for byte in bytes {
-                    let mut x_coord = self.registers[X] as usize % 64;
+                    let mut x_coord = self.registers[x] as usize % 64;
                     for bit_idx in 0..8 {
                         let mask = 0b1000_0000u8 >> bit_idx;
                         let bit = byte & mask > 0;
@@ -212,26 +228,66 @@ impl Chip8 {
                         break;
                     }
                 }
+                return DisplayState::Updated;
             }
             0xe => {
                 // skip if key
-                todo!("Not implemented");
+                let vx = self.registers[x];
+                match nn {
+                    // Skip next instruction if key with the value of Vx is pressed.
+                    0x9e => {
+                        if vx < 16 && keypresses[vx as usize] {
+                            self.pc += 2;
+                        }
+                    }
+                    // skips one instruction if the key corresponding to the value in VX is not pressed.
+                    0xa1 => {
+                        if vx >= 16 || !keypresses[vx as usize] {
+                            self.pc += 2;
+                        }
+                    }
+                    _ => panic!("Invalid instruction: {inst:#x}"),
+                }
             }
             0xf => {
-                // FX07 sets VX to the current value of the delay timer
-                // FX15 sets the delay timer to the value in VX
-                // FX18 sets the sound timer to the value in VX
-                match NN {
+                match nn {
                     // timers
-                    0x07 => self.registers[X] = self.delay_timer,
-                    0x15 => self.delay_timer = self.registers[X],
-                    0x18 => self.sound_timer = self.registers[X],
+                    // FX07 sets VX to the current value of the delay timer
+                    // FX15 sets the delay timer to the value in VX
+                    // FX18 sets the sound timer to the value in VX
+                    0x07 => self.registers[x] = self.delay_timer,
+                    0x15 => self.delay_timer = self.registers[x],
+                    0x18 => self.sound_timer = self.registers[x],
                     // add to index
-                    0x1e => self.index_reg += self.registers[X] as u16,
-                    0x0a => panic!("haven't implemented keypress instruction"),
+                    0x1e => {
+                        let prev_index = self.index_reg;
+                        self.index_reg = self.index_reg.wrapping_add(self.registers[x] as u16);
+                        if self.index_reg < prev_index {
+                            self.registers[0xf] = 1;
+                        }
+                    }
+                    // 0x1e => self.index_reg += self.registers[X] as u16,
+                    // Wait for a key press, store the value of the key in Vx.
+                    0x0a => {
+                        // The easiest way to “wait” is to decrement the PC by 2 whenever a keypad value is not detected.
+                        // This has the effect of running the same instruction repeatedly.
+                        match keypresses.iter().position(|&pressed| pressed) {
+                            Some(idx) => self.registers[x] = idx as u8,
+                            None => self.pc -= 2,
+                        }
+                    }
+                    0x29 => {
+                        // The index register I is set to the address of the hexadecimal character in VX.
+                        let vx = self.registers[x];
+                        if vx > 15 {
+                            panic!("Invalid value for VX while executing 0xfx29: {vx:?}")
+                        }
+                        let font_addr = 0x50 + 5 * vx as u16;
+                        self.index_reg = font_addr;
+                    }
                     // The interpreter takes the decimal value of Vx, and places the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.
                     0x33 => {
-                        let decimal_val = self.registers[X];
+                        let decimal_val = self.registers[x];
                         let ones = decimal_val % 10;
                         let tens = (decimal_val / 10) % 10;
                         let hundreds = decimal_val / 100;
@@ -241,13 +297,13 @@ impl Chip8 {
                     }
                     // Store registers V0 through Vx in memory starting at location I.
                     0x55 => {
-                        for i in 0..X {
+                        for i in 0..=x {
                             self.memory[self.index_reg as usize + i] = self.registers[i]
                         }
                     }
                     // Read registers V0 through Vx from memory starting at location I.
                     0x65 => {
-                        for i in 0..X {
+                        for i in 0..=x {
                             self.registers[i] = self.memory[self.index_reg as usize + i];
                         }
                     }
@@ -256,8 +312,20 @@ impl Chip8 {
             }
             _ => panic!("programming error: unhandled leading half-byte: {inst:#x}"),
         }
+        DisplayState::NotUpdated
+    }
+
+    /// Should be called at 60 Hz
+    pub fn tick_timers(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        }
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        }
+    }
+
+    pub fn is_sound_on(&self) -> bool {
+        self.sound_timer > 0
     }
 }
-
-#[test]
-fn test() {}
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,18 @@ extern crate sdl2;
 use std::error::Error;
 use std::io::ErrorKind;
 
-use chiprs::Chip8;
+use chiprs::{Chip8, DisplayState};
 
-use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::{audio, event};
 use std::env;
 use std::time::Duration;
 
-const INSTRUCTIONS_PER_SECOND: u32 = 700;
+const INSTRUCTIONS_PER_SECOND: u32 = 720;
+const INSTRUCTIONS_PER_TIMER_TICK: u32 = 720 / 60;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
@@ -23,6 +24,38 @@ fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         }
     };
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("chip-8", 640, 320)
+        .position_centered()
+        .build()
+        .expect("Unable to build sdl2 window");
+
+    // Initialize audio device for beeping
+    let audio_subsystem = sdl_context.audio().unwrap();
+    let desired_spec = sdl2::audio::AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1), // mono
+        samples: None,     // default sample size
+    };
+    let device = audio_subsystem
+        .open_playback(None, &desired_spec, |spec| {
+            // initialize the audio callback
+            SquareWave {
+                phase_inc: 440.0 / spec.freq as f32,
+                phase: 0.0,
+                volume: 0.25,
+            }
+        })
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+    canvas.present();
+
     let program = match std::fs::read(file_path) {
         Ok(program) => program,
         Err(e) => {
@@ -34,56 +67,56 @@ fn main() -> Result<(), Box<dyn Error>> {
             std::process::exit(1);
         }
     };
-
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem
-        .window("chip-8", 640, 320)
-        .position_centered()
-        .build()
-        .expect("Unable to build sdl2 window");
-
-    let mut canvas = window.into_canvas().build().unwrap();
-
-    canvas.set_draw_color(Color::BLACK);
-    canvas.clear();
-    canvas.present();
-
     let mut emulator = Chip8::load_program(&program);
     let mut event_pump = sdl_context.event_pump().unwrap();
-    'running: loop {
+    let mut inst_count = 0;
+
+    let mut keypresses = [false; 16];
+    loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
+                event::Event::Quit { .. }
+                | event::Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
-                Event::KeyDown {
+                } => return Ok(()),
+                event::Event::KeyDown {
                     keycode: Some(code),
                     ..
-                } 
-                    /*
-                    1 2 3 4
-                    q w e r
-                    a s d f
-                    z x c v
-                    */
-                => {
-                    eprintln!("Received event: {event:?}")
-                },
+                } => {
+                    if let Some(chip8_key_code) = key2btn(code) {
+                        keypresses[chip8_key_code] = true;
+                    }
+                }
+                event::Event::KeyUp {
+                    keycode: Some(key), ..
+                } => {
+                    if let Some(chip8_key_code) = key2btn(key) {
+                        keypresses[chip8_key_code] = false;
+                    }
+                }
                 _ => {}
-            }
+            };
         }
 
-        emulator.step(None);
-        render(&emulator.display, &mut canvas)?;
+        let display_state = emulator.step(keypresses);
+        if inst_count % INSTRUCTIONS_PER_TIMER_TICK == 0 {
+            emulator.tick_timers()
+        }
+        if emulator.is_sound_on() {
+            device.resume(); // Start or continue beeping
+        } else {
+            device.pause(); // Stop beeping if the sound was previously on
+        }
+        if display_state == DisplayState::Updated {
+            render(&emulator.display, &mut canvas)?;
+        }
+        inst_count = inst_count.wrapping_add(1);
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / INSTRUCTIONS_PER_SECOND));
     }
-    Ok(())
 }
 
-fn render(display: &[[bool; 64]; 32], canvas: &mut Canvas<Window>) -> Result<(), Box<dyn Error>>{
+fn render(display: &[[bool; 64]; 32], canvas: &mut Canvas<Window>) -> Result<(), Box<dyn Error>> {
     canvas.set_draw_color(Color::BLACK);
     canvas.clear();
     canvas.set_draw_color(Color::WHITE);
@@ -97,4 +130,60 @@ fn render(display: &[[bool; 64]; 32], canvas: &mut Canvas<Window>) -> Result<(),
     }
     canvas.present();
     Ok(())
+}
+
+/*
+    Keyboard                    Chip-8
+    +---+---+---+---+           +---+---+---+---+
+    | 1 | 2 | 3 | 4 |           | 1 | 2 | 3 | C |
+    +---+---+---+---+           +---+---+---+---+
+    | Q | W | E | R |           | 4 | 5 | 6 | D |
+    +---+---+---+---+     =>    +---+---+---+---+
+    | A | S | D | F |           | 7 | 8 | 9 | E |
+    +---+---+---+---+           +---+---+---+---+
+    | Z | X | C | V |           | A | 0 | B | F |
+    +---+---+---+---+           +---+---+---+---+
+*/
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
+}
+
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32,
+}
+
+impl audio::AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        // Generate a square wave
+        for x in out.iter_mut() {
+            *x = if self.phase <= 0.5 {
+                self.volume
+            } else {
+                -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
 }
